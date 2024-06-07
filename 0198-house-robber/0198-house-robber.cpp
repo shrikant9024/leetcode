ec1acb383089226e85567f4a95724483a032df83
Shrikant Ramani <86430772+shrikant9024@users.noreply.github.com>
2024-06-07 05:31:37 +0530

    Time: 2 ms (40.19%), Space: 9.1 MB (81.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1,0);
-        //base cond- i==n, i>1
-        
-        dp[0] = nums[0];
-        
+        int prev = nums[0];
+        int prev2 = 0;        
         for(int i =1;i<n;i++){
-            int notpick = dp[i-1];
+            int notpick = prev;
             int pick = nums[i];
             if(i>1){
-                pick+= dp[i-2];
+                pick+= prev2;
             }
-            dp[i] = max(pick,notpick);
+            int cur = max(pick,notpick);
+            
+            prev2= prev;
+            prev = cur;
         }
-        return dp[n-1];
+        return prev;
         
     }
 };
