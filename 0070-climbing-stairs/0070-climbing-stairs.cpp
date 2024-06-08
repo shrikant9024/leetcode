99f375a366eb2e6759f8d17f0e7eb2d6bbb0c741
Shrikant Ramani <86430772+shrikant9024@users.noreply.github.com>
2024-06-08 15:34:58 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (11.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int f(int i,vector<int> &dp){
-        if(i==0) return 1;
-        if(i<1) return 1;
-        if(dp[i]!=-1) return dp[i];
-        
-        int step1 = f(i-1,dp);
-        int step2 = f(i-2,dp);
-        
-        dp[i] =  step1+step2;
-        
-        return dp[i];
-    }
+    
     int climbStairs(int n) {
-    vector<int> dp(n,-1);
-        return f(n-1,dp);
+    vector<int> dp(n+1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i =2;i<=n;i++){
+            
+                int step1 = dp[i-1];
+                int step2 = dp[i-2];
+                dp[i] = step1+step2;
+            
+            
+        }
+        return dp[n];
     }
 };
