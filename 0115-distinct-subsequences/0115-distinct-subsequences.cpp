c884d7f3bb9fbe2e39df71f6a21666abdd81b0c7
Shrikant Ramani <86430772+shrikant9024@users.noreply.github.com>
2024-06-11 17:13:10 +0530

    Time: 8 ms (93.36%), Space: 7.7 MB (91.99%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    
-
     int numDistinct(string s, string t) {
         int m = s.size();
         int n = t.size();
-        vector<vector<double>>dp(m+1, vector<double>(n+1,0));
+        vector<double> prev(n + 1, 0);
         
-        for(int i =0;i<=m;i++) dp[i][0] = 1;
-
+        prev[0] = 1;  // When t is an empty string, there's exactly one subsequence in any prefix of s that matches it: the empty subsequence
         
-        for(int i =1;i<=m;i++){
-            for(int j =1;j<=n;j++){
-                if(s[i-1]==t[j-1]){
-                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
-                }
-                else dp[i][j]  = dp[i-1][j];
+        for (int i = 1; i <= m; i++) {
+            for (int j = n; j >= 1; j--) {
+                if (s[i - 1] == t[j - 1]) {
+                    prev[j] = prev[j - 1] + prev[j];
+                } 
             }
         }
-        return (int)dp[m][n];
+        
+        return (int)prev[n];
     }
 };
