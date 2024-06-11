23f2daefcd71dc1249851ed3b8cf03ae1743ea38
Shrikant Ramani <86430772+shrikant9024@users.noreply.github.com>
2024-06-11 17:13:17 +0530

    Time: 4 ms (98.26%), Space: 7.8 MB (87.20%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -5,7 +5,7 @@ public:
         int n = t.size();
         vector<double> prev(n + 1, 0);
         
-        prev[0] = 1;  // When t is an empty string, there's exactly one subsequence in any prefix of s that matches it: the empty subsequence
+        prev[0] = 1;  
         
         for (int i = 1; i <= m; i++) {
             for (int j = n; j >= 1; j--) {
