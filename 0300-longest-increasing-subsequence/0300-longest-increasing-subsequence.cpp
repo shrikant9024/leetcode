64056c7564b71b8f705d288bcbe7b9599960181d
Shrikant Ramani <86430772+shrikant9024@users.noreply.github.com>
2024-06-12 22:31:35 +0530

    Time: 0 ms (100.00%), Space: 12.8 MB (64.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-       vector<int> next(n+1,0),cur(n+1,0);
-        for(int i = n-1;i>=0;i--){
-            for(int prev = i-1;prev>=-1;prev--){
-                
-                int len = next[prev+1];
-                if(prev==-1 || nums[prev]<nums[i]){
-                    len = max(len,1+ next[i+1]);
-                }
-               cur[prev+1] = len;
-            }
-            next = cur;
+    int lengthOfLIS(vector<int>& arr) {
+            vector<int> temp;
+            int n = arr.size();
+    temp.push_back(arr[0]);		//push first element of array into vector
+    int len = 1;				//take Len =1
+    for(int i =1;i<n;i++){			//iterate in array
+        if(arr[i]>temp.back()){		//check if element is greater than the last element pushed in vector
+            temp.push_back(arr[i]);	//if yes push in vector
+            len++;					//increment l
+        } else{
+            int ind = lower_bound(temp.begin(),temp.end(),arr[i]) - temp.begin();	//imply lower bound func
+//{	when u apply lower bound func. U point to the next greater element and it index which is to found. Suppose array is [1,2.4,6] so if element to found is 4 at index 2. . Lower bound will point to 6  so u do -temp.begin so it points to 4 again.
+//}
+            temp[ind] = arr[i];		//u have gotten the index now update in vector
+
         }
-        return cur[0];
+    }
+    return len;
+
     }
 };
