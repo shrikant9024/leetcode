37a49330a888109eba748d7d6195f5c684692f49
Shrikant Ramani <86430772+shrikant9024@users.noreply.github.com>
2024-07-08 00:14:14 +0530

    Time: 322 ms (57.76%), Space: 135.2 MB (62.80%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
-    int f(int i, int buy, int cap,vector<int>&prices){
-        int n=prices.size();
-        if(cap==0) return 0;
-        if(i==n) return 0;
+    int f(int i, int trans,vector<int>&prices,vector<vector<int>>&dp){
+        int n = prices.size();
+        if(i==n || trans==4) return 0;
         
+        if(dp[i][trans]!=-1) return dp[i][trans];
         int profit = 0;
-        if(buy){
-            profit = max(-prices[i]+f(i+1,0,cap,prices),f(i+1,1,cap,prices));
+        if(trans%2==0){
+            profit = max(-prices[i]+f(i+1,trans+1,prices,dp),f(i+1,trans,prices,dp));
+        }
+        else {
+            profit = max(prices[i]+f(i+1,trans+1,prices,dp),f(i+1,trans,prices,dp));
             
         }
-        else{
-            profit = max(prices[i]+f(i+1,1,cap-1,prices),f(i+1,0,cap,prices));
-            
-        }
-        
-        return profit;
+        dp[i][trans] = profit;
+        return dp[i][trans];
         
     }
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        
-        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2,vector<int>(3,0)));
-            
-        for(int i =n-1;i>=0;i--){
-            for(int buy = 0;buy<=1;buy++){
-                for(int cap=1;cap<=2;cap++){
-        int profit = 0;
-        if(buy){
-            profit = max(-prices[i]+dp[i+1][0][cap],dp[i+1][1][cap]);
-            
-        }
-        else{
-            profit = max(prices[i]+dp[i+1][1][cap-1],dp[i+1][0][cap] );
-            
-        }
-                    dp[i][buy][cap] = profit;
-                }
-            }
-        }
-        return dp[0][1][2];
+        int n =prices.size();
+        vector<vector<int>>dp(n,vector<int>(4,-1));
+        return f(0,0,prices,dp);
     }
 };
