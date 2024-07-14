a2dd4dd37b2dc9dc02adf3f10356ca53c0834e52
Shrikant Ramani <86430772+shrikant9024@users.noreply.github.com>
2024-07-14 23:11:36 +0530

    Time: 161 ms (59.19%), Space: 90.5 MB (10.71%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,21 +1,22 @@
 class StockSpanner {
 public:
     stack<pair<int,int>> st;
-    
     StockSpanner() {
         
     }
     
     int next(int price) {
-        
         int span = 1;
-        while(!st.empty() && st.top().first <= price){
-            span +=st.top().second;
+        
+        while(!st.empty() && st.top().first<=price){
+            span = span + st.top().second;
             st.pop();
-            
         }
+        
         st.push({price,span});
         return span;
+        
+        
     }
 };
 
